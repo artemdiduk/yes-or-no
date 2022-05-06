1a8e549bdbccfb89aba3f12ca1bb7f144ff68196
artemdiduk <68499262+artemdiduk@users.noreply.github.com>
2022-05-07 01:13:34 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,12 +4,16 @@ let img = document.querySelector('.random__img')
 let mes = ['YES', 'NO'];
 
 btn.addEventListener('click', ()=> {
+    result.innerHTML = ``;
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min)) + min;
     }
-    result.innerHTML = `${mes[getRandomInt(0, 2)]}`
+    setTimeout(function(){
+        result.innerHTML = `${mes[getRandomInt(0, 2)]}`
+    },320)
+    
     img.classList.add('active');
     setTimeout(function(){
     img.classList.remove('active');
-    },300)
+    },350)
 })
